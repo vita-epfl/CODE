d22b05d2747db5d126ccd0e9228e2180ee278b91
Bastien Van Delft <bastienvandelft@epfl.ch>
2023-04-14 17:26:33 +0200

    first commit

---
+++ ddpm/datasets/__init__.py
@@ -0,0 +1,289 @@
+import os
+import torch
+import numbers
+import torchvision.transforms as transforms
+import torchvision.transforms.functional as F
+from torchvision.datasets import CIFAR10
+from ddpm.datasets.cityscape import Cityscapes_Pretraining_Dataset, Cityscapes
+from ddpm.datasets.celeba import CelebA
+from ddpm.datasets.ffhq import FFHQ
+from ddpm.datasets.lsun import LSUN
+from ddpm.datasets.audio import AudioDataset
+from torch.utils.data import Subset
+import numpy as np
+
+
+class Crop(object):
+    def __init__(self, x1, x2, y1, y2):
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+
+    def __call__(self, img):
+        return F.crop(img, self.x1, self.y1, self.x2 - self.x1, self.y2 - self.y1)
+
+    def __repr__(self):
+        return self.__class__.__name__ + "(x1={}, x2={}, y1={}, y2={})".format(
+            self.x1, self.x2, self.y1, self.y2
+        )
+
+
+def get_dataset(args, cfg):
+    if args is None:
+        args = cfg.trainer
+    dataset_name = args.dataset
+    datapath = args.datapath
+    corruption_severity = args.corruption_severity
+    corruption = args.corruption
+    random_flip = args.random_flip
+    lower_image_size = args.lower_image_size
+    image_size = args.img_size
+
+    print('dataset_name',dataset_name)
+    if random_flip is False:
+        train_transform = test_transform = transforms.Compose(
+            [transforms.Resize(image_size), transforms.ToTensor()]
+        )
+    else:
+        train_transform = transforms.Compose(
+            [
+                transforms.Resize(image_size),
+                transforms.RandomHorizontalFlip(p=0.5),
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        )
+        test_transform = transforms.Compose(
+            [transforms.Resize(image_size), 
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+        )
+
+    if dataset_name == "CIFAR10":
+        dataset = CIFAR10(
+            os.path.join('./data', "datasets", "cifar10"),
+            train=True,
+            download=True,
+            transform=train_transform,
+        )
+        test_dataset = CIFAR10(
+            os.path.join('./data', "datasets", "cifar10_test"),
+            train=False,
+            download=True,
+            transform=test_transform,
+        )
+
+    elif dataset_name == "CELEBA":
+        cx = 89
+        cy = 121
+        x1 = cy - 64
+        x2 = cy + 64
+        y1 = cx - 64
+        y2 = cx + 64
+        if random_flip:
+            dataset = CelebA(
+                root=datapath,
+                split="train",
+                corruption=corruption,
+                corruption_severity=corruption_severity,
+                transform=transforms.Compose(
+                    [
+                        Crop(x1, x2, y1, y2),
+                        transforms.Resize(image_size),
+                        transforms.RandomHorizontalFlip(),
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                    ]
+                ),
+                download=False,
+            )
+
+        else:
+            dataset = CelebA(
+                root=datapath,#os.path.join(args.exp, "datasets", "celeba"),
+                split="train",
+                corruption=corruption,
+                corruption_severity=corruption_severity,
+                transform=transforms.Compose(
+                    [
+                        Crop(x1, x2, y1, y2),
+                        transforms.Resize(image_size),
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                    ]
+                ),
+                download=False,
+            )
+
+        test_dataset = CelebA(
+            root=datapath, #os.path.join(args.exp, "datasets", "celeba"),
+            split="test",
+            corruption=corruption,
+            corruption_severity=corruption_severity,
+            transform=transforms.Compose(
+                [
+                    Crop(x1, x2, y1, y2),
+                    transforms.Resize(image_size),
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                ]
+            ),
+            download=False,
+        )
+    elif dataset_name == "MAESTRO":
+        print("dataset_name", dataset_name)
+        dataset = AudioDataset(cfg)
+        if dataset.should_build_dataset():
+            dataset.build_collated()
+        test_dataset = None
+
+    elif dataset_name == "CITYSCAPES_ORIGINAL":
+        if cfg.dataset.mode == "fine":
+            dataset = Cityscapes(root=cfg.dataset.root_dir, split='train', mode=cfg.dataset.mode,
+                     target_type='semantic', transforms=transforms.Compose([transforms.ToTensor(),]),)
+# transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))   
+            val_dataset = Cityscapes(root=cfg.dataset.root_dir, split='val', mode=cfg.dataset.mode,
+                     target_type='semantic', transforms=transforms.Compose([transforms.ToTensor(),]),)
+            
+            dataset = torch.utils.data.ConcatDataset([train_dataset, val_dataset])
+
+            test_dataset = Cityscapes(root=cfg.dataset.root_dir, split='test', mode=cfg.dataset.mode,
+                     target_type='semantic', transforms=transforms.Compose([transforms.ToTensor(),]),)
+
+        elif cfg.dataset.mode == "coarse":
+            raise NotImplementedError
+
+    elif dataset_name == "CITYSCAPES_PRETRAINING":
+        if cfg.trainer.random_flip:
+            train_transform =  transforms.Compose(
+                [
+                    transforms.Resize(lower_image_size),
+                    transforms.RandomHorizontalFlip(p=0.5),
+                    transforms.RandomCrop(image_size),
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                ]
+            )
+            test_transform = transforms.Compose(
+                [transforms.Resize(lower_image_size), 
+                transforms.RandomCrop(image_size),
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+            )
+        dataset = Cityscapes_Pretraining_Dataset(cfg, transform = train_transform)
+        test_dataset = Cityscapes_Pretraining_Dataset(cfg,transform = test_transform, split = "test")
+    
+    elif dataset_name == "CITYSCAPES":
+        dataset = Cityscapes_Pretraining_Dataset(cfg)
+        test_dataset = Cityscapes_Pretraining_Dataset(cfg, split = "test")
+
+    elif dataset_name == "LSUN":
+        train_folder = "{}_train".format(config.data.category)
+        val_folder = "{}_val".format(config.data.category)
+        if config.data.random_flip:
+            dataset = LSUN(
+                root=os.path.join(args.exp, "datasets", "lsun"),
+                classes=[train_folder],
+                transform=transforms.Compose(
+                    [
+                        transforms.Resize(config.data.image_size),
+                        transforms.CenterCrop(config.data.image_size),
+                        transforms.RandomHorizontalFlip(p=0.5),
+                        transforms.ToTensor(),
+                    ]
+                ),
+            )
+        else:
+            dataset = LSUN(
+                root=os.path.join(args.exp, "datasets", "lsun"),
+                classes=[train_folder],
+                transform=transforms.Compose(
+                    [
+                        transforms.Resize(config.data.image_size),
+                        transforms.CenterCrop(config.data.image_size),
+                        transforms.ToTensor(),
+                    ]
+                ),
+            )
+
+        test_dataset = LSUN(
+            root=os.path.join(args.exp, "datasets", "lsun"),
+            classes=[val_folder],
+            transform=transforms.Compose(
+                [
+                    transforms.Resize(config.data.image_size),
+                    transforms.CenterCrop(config.data.image_size),
+                    transforms.ToTensor(),
+                ]
+            ),
+        )
+
+    elif dataset_name == "FFHQ":
+        if config.data.random_flip:
+            dataset = FFHQ(
+                path=os.path.join(args.exp, "datasets", "FFHQ"),
+                transform=transforms.Compose(
+                    [transforms.RandomHorizontalFlip(p=0.5), transforms.ToTensor()]
+                ),
+                resolution=config.data.image_size,
+            )
+        else:
+            dataset = FFHQ(
+                path=os.path.join(args.exp, "datasets", "FFHQ"),
+                transform=transforms.ToTensor(),
+                resolution=config.data.image_size,
+            )
+
+        num_items = len(dataset)
+        indices = list(range(num_items))
+        random_state = np.random.get_state()
+        np.random.seed(2019)
+        np.random.shuffle(indices)
+        np.random.set_state(random_state)
+        train_indices, test_indices = (
+            indices[: int(num_items * 0.99)],
+            indices[int(num_items * 0.9) :],
+        )
+        test_dataset = Subset(dataset, test_indices)
+        dataset = Subset(dataset, train_indices)
+    
+    else:
+        LOG.warning(f"No dataset named {dataset_name}")
+        dataset, test_dataset = None, None
+
+    return dataset, test_dataset
+
+
+def logit_transform(image, lam=1e-6):
+    image = lam + (1 - 2 * lam) * image
+    return torch.log(image) - torch.log1p(-image)
+
+
+def data_transform(config, X):
+    if config.data.uniform_dequantization:
+        X = X / 256.0 * 255.0 + torch.rand_like(X) / 256.0
+    if config.data.gaussian_dequantization:
+        X = X + torch.randn_like(X) * 0.01
+
+    if config.data.rescaled:
+        X = 2 * X - 1.0
+    elif config.data.logit_transform:
+        X = logit_transform(X)
+
+    if hasattr(config, "image_mean"):
+        return X - config.image_mean.to(X.device)[None, ...]
+
+    return X
+
+
+def inverse_data_transform(config, X):
+    if hasattr(config, "image_mean"):
+        X = X + config.image_mean.to(X.device)[None, ...]
+
+    if config.data.logit_transform:
+        X = torch.sigmoid(X)
+    elif config.data.rescaled:
+        X = (X + 1.0) / 2.0
+
+    return torch.clamp(X, 0.0, 1.0)
---
+++ ddpm/trainers/base_trainer.py
@@ -0,0 +1,266 @@
+from abc import abstractmethod
+import logging
+import os
+import sys
+from pathlib import Path
+from dataclasses import dataclass
+from typing import Dict, List, Tuple, Any, Optional, Union
+from omegaconf import DictConfig, OmegaConf, open_dict
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+from torch.cuda import init
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel
+import torchaudio
+import hashlib
+import subprocess
+import submitit
+from tqdm import tqdm
+import time
+import datetime
+
+from torch.utils.tensorboard import SummaryWriter
+
+from ddpm.datasets.audio import create_dataloader, AudioDataset
+from ddpm.utils.utils import fix_random_seeds
+# from clearaudio.models.dummy_model import DummyModel
+
+LOG = logging.getLogger(__name__)
+
+
+def is_dist_avail_and_initialized():
+    if not dist.is_available():
+        return False
+    if not dist.is_initialized():
+        return False
+    return True
+
+
+def get_world_size():
+    if not is_dist_avail_and_initialized():
+        return 1
+    return dist.get_world_size()
+
+
+def get_rank():
+    if not is_dist_avail_and_initialized():
+        return 0
+    return dist.get_rank()
+
+
+def is_main_process():
+    return get_rank() == 0
+
+
+def setup_for_distributed(is_master):
+    """
+    This function disables printing when not in master process
+    """
+    import builtins as __builtin__
+
+    builtin_print = __builtin__.print
+
+    def print(*args, **kwargs):
+        force = kwargs.pop("force", False)
+        if is_master or force:
+            builtin_print(*args, **kwargs)
+
+    __builtin__.print = print
+
+
+def init_local_distributed_mode(cfg: DictConfig) -> DictConfig:
+    with open_dict(cfg):  # add to config
+        # launched with torch.distributed.launch locally
+        if "RANK" in os.environ and "WORLD_SIZE" in os.environ:
+            cfg.trainer.rank = int(os.environ["RANK"])
+            cfg.trainer.world_size = int(os.environ["WORLD_SIZE"])
+            cfg.trainer.gpu = int(os.environ["LOCAL_RANK"])
+        elif torch.cuda.is_available():
+            LOG.info("Will run the code on one GPU.")
+            # Naive launch
+            # we manually add MASTER_ADDR and MASTER_PORT to env variables
+            cfg.trainer.rank, cfg.trainer.gpu, cfg.trainer.world_size = 0, 0, 1
+            os.environ["MASTER_ADDR"] = "127.0.0.1"
+            os.environ["MASTER_PORT"] = "29400"
+        else:
+            LOG.error("Does not support training without GPU.")
+            sys.exit(1)
+
+    # LOG.info(f'Init process group: {cfg.trainer}')
+    dist.init_process_group(
+        backend="nccl",
+        init_method="env://",
+        world_size=cfg.trainer.world_size,
+        timeout=datetime.timedelta(seconds=1800),
+        rank=cfg.trainer.rank,
+    )
+
+    torch.cuda.set_device(cfg.trainer.gpu)
+    # LOG.info('| distributed init (rank {}): {}'.format(cfg.trainer.rank, cfg.trainer.gpu))
+    dist.barrier()
+    return cfg
+
+
+class BaseTrainer(object):
+    current_epoch = 0
+    model = None
+    optimizer = None
+
+    def __init__(self, cfg: DictConfig) -> None:
+        self.cfg = cfg
+
+    def setup_local(self) -> None:
+        torchaudio.set_audio_backend("sox_io")
+        self.cfg = init_local_distributed_mode(self.cfg)
+
+    def run(self) -> None:
+        self.train()
+        # if self.cfg.trainer.mode == "eval":
+        #     self.eval()
+        # elif self.cfg.trainer.mode == "train":
+        #     self.train()
+        # else:
+        #     raise NotImplementedError
+
+    @abstractmethod
+    def train(self) -> None:
+        pass
+
+    @abstractmethod
+    def setup_trainer(self) -> None:
+        pass
+
+    def setup_platform(self) -> None:
+        fix_random_seeds(self.cfg.trainer.seed)
+        if self.cfg.trainer.platform == "local":
+            LOG.info(f"Training platform : {self.cfg.trainer.platform}")
+            self.setup_local()
+        elif self.cfg.trainer.platform == "slurm":
+            LOG.info(f"Training platform : {self.cfg.trainer.platform}")
+            self.setup_slurm()
+        else:
+            raise NotImplementedError("Unknown platform (valid value are local or slurm)")
+
+    @abstractmethod
+    def eval(self) -> None:
+        pass
+
+    # https://github.com/facebookincubator/submitit/blob/main/docs/checkpointing.md
+    def __call__(self) -> None:
+        self.setup_platform()
+        self.setup_trainer()
+        self.run()
+
+    def setup_slurm(self) -> None:
+        # torchaudio.set_audio_backend("soundfile")
+        torchaudio.set_audio_backend("sox_io")
+        job_env = submitit.JobEnvironment()
+        with open_dict(self.cfg):
+            self.cfg.trainer.job_id = job_env.job_id
+            self.cfg.trainer.gpu = job_env.local_rank
+            self.cfg.trainer.rank = job_env.global_rank
+            self.cfg.trainer.world_size = job_env.num_tasks
+        LOG.info(f"Process group: {job_env.num_tasks} tasks, rank: {job_env.global_rank}")
+        LOG.error(f"Process group: {job_env.num_tasks} tasks, rank: {job_env.global_rank}")
+        ### Master address & Port
+        if "MASTER_PORT" in os.environ:
+            pass  # use MASTER_PORT in the environment variable
+        else:
+            # from PyTorch Lightning
+            default_port = os.environ.get("SLURM_JOB_ID")
+            job_id = default_port
+            if default_port:
+                # use the last 4 numbers in the job id as the id
+                default_port = default_port[-4:]
+                # all ports should be in the 10k+ range
+                default_port = int(default_port) + 15000
+            else:
+                default_port = 12910
+            os.environ["MASTER_PORT"] = str(default_port)
+
+        # Master Addr
+        node_list = os.environ["SLURM_NODELIST"]
+        addr = subprocess.getoutput(f"scontrol show hostname {node_list} | head -n1")
+        if "MASTER_ADDR" not in os.environ:
+            os.environ["MASTER_ADDR"] = addr
+        os.environ["WORLD_SIZE"] = str(self.cfg.trainer.world_size)  # str(ntasks)
+        os.environ["LOCAL_RANK"] = str(self.cfg.trainer.gpu)  # str(proc_id % num_gpus)
+        os.environ["RANK"] = str(self.cfg.trainer.rank)  # str(proc_id)
+        print("WORLD SIZE :", self.cfg.trainer.world_size)
+        print("LOCAL_RANK :", self.cfg.trainer.gpu)
+        print("RANK :", self.cfg.trainer.rank)
+        dist.init_process_group(
+            backend="nccl",
+            init_method="env://",
+            world_size=self.cfg.trainer.world_size,
+            rank=self.cfg.trainer.rank,
+        )
+        torch.cuda.set_device(self.cfg.trainer.gpu)
+        dist.barrier()
+
+    def checkpoint(self) -> submitit.helpers.DelayedSubmission:
+        print("Requeuing SLURM job", OmegaConf.to_yaml(self.cfg))
+        empty_trainer = type(self)(self.cfg)
+        if (self.model is not None) and (self.optimizer is not None):
+            self.checkpoint_dump(
+                checkpoint_path=self.cfg.trainer.checkpointpath,
+                epoch=self.current_epoch,
+                model_state_dict=self.model.state_dict(),
+                optimizer_state_dict=self.optimizer.state_dict(),
+            )
+        else:
+            pass
+        print("Sending Delayed Submission...")
+        return submitit.helpers.DelayedSubmission(empty_trainer)
+
+    def checkpoint_dump(
+        self,
+        checkpoint_path: str = None,
+        epoch: int = 0,
+        model_state_dict: Dict = None,
+        optimizer_state_dict: Dict = None,
+        **kwargs,
+    ) -> None:
+        if (model_state_dict is None) and (self.model is not None):
+            model_state_dict = self.model.state_dict()
+
+        if (optimizer_state_dict is None) and (self.optimizer is not None):
+            optimizer_state_dict = self.optimizer.state_dict()
+
+        if checkpoint_path is None:
+            prefix = self.cfg.trainer.output_dir
+            if self.cfg.trainer.platform == "local": # Hydra changes base directory
+                prefix = ''
+            if epoch == 0:
+                checkpoint_path = os.path.join(prefix, "default_checkpoint.pt")
+            else:
+                checkpoint_path = os.path.join(prefix, f"checkpoint_epoch_{str(epoch)}.pt")
+
+        torch.save(
+            {
+                "epoch": epoch,
+                "model_state_dict": model_state_dict,
+                "optimizer_state_dict": optimizer_state_dict,
+                "config": OmegaConf.to_container(self.cfg, resolve=True),
+                **kwargs
+            },
+            checkpoint_path,
+        )
+
+    def checkpoint_load(self, checkpoint_path: Union[str, Path]) -> Optional[Dict]:
+        if not checkpoint_path:
+            return None 
+        if not Path(checkpoint_path).exists():
+            return None
+        return torch.load(str(checkpoint_path))
+
+    def find_latest_checkpoint_path(self, output_dir: Union[str, Path]) -> Optional[Path]:
+        p = Path(output_dir)
+        if not p.exists():
+            return None
+        checkpoints = [x for x in p.iterdir() if x.is_file() and x.suffix == '.pt']
+        if not checkpoints:
+            return None
+        checkpoints.sort(key=lambda x: int(x.stem.split('_')[-1]), reverse=True)
+        return checkpoints[0]
